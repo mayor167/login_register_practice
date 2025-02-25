482dbc1ac6b992f06a877dc3267abccf23cd4f86
mayor167 <adenijimayokun@gmail.com>
2025-02-25 20:10:47 +0100

    add

---
+++ ooplr/core/init.php
@@ -0,0 +1,22 @@
+<?php
+$GLOBALS['config'] = array(
+  'mysql' => array(
+    'host' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '',
+     'dbname' => 'login_register_test'
+  ),
+  'remember' => array(
+    'cookie_name' => 'hash',
+    'cookie_expiry' => 604800
+  ),
+  'session' => array(
+    'session_name' => 'user'
+  )
+);
+spl_autoload_register(function($class){
+require_once 'classes/'. $class. 'php';
+}
+
+)
+?>
